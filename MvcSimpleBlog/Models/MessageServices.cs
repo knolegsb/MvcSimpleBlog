883e234c73dd943917dc1d28bb99a58697ac145c
knolegsb <seon2you@hotmail.com>
2015-10-26 09:59:32 -0700

    added message services to model

---
+++ MvcSimpleBlog/Models/MessageServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace MvcSimpleBlog.Models
+{
+    public class MessageServices
+    {
+        public async static TaskEventHandler SendEmailAsync(string email, string subject, string message)
+        {
+            try
+            {
+                var _email = "seon2you@hotmail.com";
+                var _epass = ConfigurationManager.AppSettings["EmailPassword"];
+                var _dispName = "Sean";
+                MailMessage myMessage = new MailMessage();
+                myMessage.To.Add(email);
+                myMessage.From = new MailAddress(_email, _dispName);
+                myMessage.Subject = subject;
+                myMessage.Body = message;
+                myMessage.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.EnableSsl = true;
+                    smtp.Host = "smtp.live.com";
+                    smtp.Port = 587;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(_email, _epass);
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.SendCompleted += (s, e) => { smtp.Dispose(); };
+                    await smtp.SendMailAsync(myMessage);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
